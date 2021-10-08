fe494f74c6edf8e50e27e2525a7deea04c6e11fb
Arielv1 <arielv1994@gmail.com>
2021-10-08 15:01:59 +0300

    Initial frontend & backend setup

---
+++ starter/frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="note in notes" :key="note">
+      {{note}}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data: () => ({
+    notes: []
+  }),
+  components: {
+    
+  },
+  mounted () {
+  axios.get('http://localhost:8000/api/notes/').then((response) => {
+    this.notes = response.data;
+  })
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
